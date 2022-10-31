6af29666c8a67ea4606b4e4c585dfd2fc544748a
Lambourl <lmbrluc@gmail.com>
2022-10-31 09:09:43 +0100

    :wink: add main.cpp

---
+++ help.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+
+inline void display_an_message(std::string message){
+    std::cout<<message<<std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <help.hpp>
+
+int main(){
+    display_an_message("hello world");
+    return 0;
+}
